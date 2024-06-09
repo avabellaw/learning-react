142dea8de16466b728cc2a6e1e2429e018a146e6
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-06-09 23:35:26 +0100

    Add a stateless functional component

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+
+const FunctionalGreeting = () => <h1>hello from react</h1>;
+
+export default FunctionalGreeting;
---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+
+const FunctionalGreetingWithProps = (props) => <h1>hello {props.name} from react</h1>;
+
+export default FunctionalGreetingWithProps;
