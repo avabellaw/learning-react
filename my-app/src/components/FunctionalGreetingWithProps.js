952a78ba6b15ab49aa1504e5e00468b085994454
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-06-11 14:57:36 +0100

    Add stateless function with props

--- my-app/src/components/FunctionalGreetingWithProps.js
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 
-const FunctionalGreetingWithProps = (props) => <h1>hello {props.name} from react</h1>;
+const FunctionalGreetingWithProps = (props) => <h1>Hi {props.name}! I see you're {props.age} years old; {props.greeting}</h1>;
 
 export default FunctionalGreetingWithProps;
